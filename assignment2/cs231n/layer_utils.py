60d085bd4ed4d54c7ec82b3ef72806321bdcba1b
Dimitrios <d.psychogyios@gmail.com>
2019-06-14 20:55:43 +0300

    Add generic layer, to perform dropout and/or batch normalization based on input parameters.

--- assignment2/cs231n/layer_utils.py
+++ assignment2/cs231n/layer_utils.py
@@ -59,6 +59,61 @@ def affine_bn_relu_backward(dout, cache):
     dx, dw, db = affine_backward(da, fc_cache)
     return dx, dw, db, dgamma, dbeta
 
+def affine_generic_forward(x, w, b, gamma, beta, bn_param, dropout_param):
+    """
+    Convenience layer that perorms an affine transform followed by a ReLU
+
+    Inputs:
+    - x: Input to the affine layer
+    - w, b: Weights for the affine layer
+
+    Returns a tuple of:
+    - out: Output from the ReLU
+    - cache: Object to give to the backward pass
+    """
+    # Check if batch normalization and/or dropout is needed
+    # if parameter dictionarys are empty, bool(dict)= False
+    use_batch_norm = bool(bn_param)
+    use_dropout = bool(dropout_param)
+    
+    #In case batch norm and/or dropout is not needed, we pass empty
+    # tuples in cache
+    bn_cache = ()
+    do_cache = ()
+    
+    out, fc_cache = affine_forward(x, w, b)
+    if (use_batch_norm):
+        out, bn_cache = batchnorm_forward(out, gamma, beta, bn_param)
+    out, relu_cache = relu_forward(out)
+    
+    if (use_dropout):
+        out, do_cache = dropout_forward(out, dropout_param)
+    
+    cache = (fc_cache, bn_cache, relu_cache, do_cache)
+    return out, cache
+
+
+def affine_generic_backward(dout, cache):
+    """
+    Backward pass for the affine-relu convenience layer
+    """
+    fc_cache, bn_cache, relu_cache, do_cache = cache
+    
+    # Check if batch normalization and/or dropout is needed
+    # if cache tuples are empty, bool(tuple)= False
+    use_batch_norm = bool(bn_cache)
+    use_drop_out = bool(do_cache)
+    dgamma =None
+    dbeta = None
+    
+    if (use_drop_out):
+        dout = dropout_backward(dout, do_cache)
+    dout = dbn = relu_backward(dout, relu_cache)
+    if use_batch_norm :
+        dout, dgamma, dbeta = batchnorm_backward(dbn, bn_cache)
+    dx, dw, db = affine_backward(dout, fc_cache)
+    return dx, dw, db, dgamma, dbeta
+
 
 def conv_relu_forward(x, w, b, conv_param):
     """
